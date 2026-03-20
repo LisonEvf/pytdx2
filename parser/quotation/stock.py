8fc6949a5146eb6f9cbb0c05514419534741465c
huanghuibin <huanghuibin@gmail.com>
2026-03-20 16:59:16 +0800

    fixeed

--- parser/quotation/stock.py
+++ parser/quotation/stock.py
@@ -1,5 +1,4 @@
 from parser.quotation.auction import Auction
-from parser.quotation.board_list import BoardList
 from parser.quotation.chart_sampling import ChartSampling
 from parser.quotation.count import Count
 from parser.quotation.history_orders import HistoryOrders
@@ -22,28 +21,27 @@ from parser.quotation.unusual import Unusual
 from parser.quotation.volume_profile import VolumeProfile
 
 __all__ = [
-    'Auction',
-    'BoardList',
-    'ChartSampling',
-    'Count',
-    'HistoryOrders',
-    'HistoryTickChart',
-    'HistoryTransactionWithTrans',
-    'HistoryTransaction',
-    'IndexInfo',
-    'IndexMomentum',
-    'K_Line_Offset',
-    'K_Line',
-    'List',
-    'List2',
-    'QuotesDetail',
-    'QuotesList',
-    'Quotes',
-    'TickChart',
-    'TopBoard',
-    'Transaction',
-    'Unusual',
-    'VolumeProfile',
+    "Auction",
+    "ChartSampling",
+    "Count",
+    "HistoryOrders",
+    "HistoryTickChart",
+    "HistoryTransactionWithTrans",
+    "HistoryTransaction",
+    "IndexInfo",
+    "IndexMomentum",
+    "K_Line_Offset",
+    "K_Line",
+    "List",
+    "List2",
+    "QuotesDetail",
+    "QuotesList",
+    "Quotes",
+    "TickChart",
+    "TopBoard",
+    "Transaction",
+    "Unusual",
+    "VolumeProfile",
 ]
 
 from const import MARKET
@@ -52,27 +50,25 @@ import struct
 from typing import override
 
 
-    
 @register_parser(0x452)
 class f452(BaseParser):
-    def __init__(self, start:int = 0, count:int = 2000):
-        self.body = struct.pack('<IIIH', start, count, 1, 0)
+    def __init__(self, start: int = 0, count: int = 2000):
+        self.body = struct.pack("<IIIH", start, count, 1, 0)
 
     @override
     def deserialize(self, data):
-        count, = struct.unpack('<H', data[:2])
+        (count,) = struct.unpack("<H", data[:2])
         result = []
         for i in range(count):
-            market, code_num, p1, p2 = struct.unpack('<BIff', data[i * 13 + 2: i * 13 + 15])
-            result.append({
-                'market': MARKET(market),
-                'code': f'{code_num}',
-                'p1': p1,
-                'p2': p2
-            })
+            market, code_num, p1, p2 = struct.unpack(
+                "<BIff", data[i * 13 + 2 : i * 13 + 15]
+            )
+            result.append(
+                {"market": MARKET(market), "code": f"{code_num}", "p1": p1, "p2": p2}
+            )
 
         return result
-    
+
 
 # >0700
 # 01 393939393939 c437 0200 # 01 393939393939 e355 0200
@@ -87,30 +83,31 @@ class f452(BaseParser):
 # 92aaaaaaaaaaaa
 # 1f812b7bbc50b941891a97478970c69193d9233a48619093b572a4c0054d18669229585c6e9213365ad20c7a4e900d5a3a8d9334800f9c0c7701973b5a91bd953a083e7d9293089d289c939393939393939391939393939393939393
 # 6b7bbc6b7bbc93936b7bbc6b7bbc93936b7bbc6b7bbc93936b7bbc6b7bbc93936b7bbc6b7bbc93936b7bbc6b7bbc9393
-# 
+#
 # 93a0aaaaa3a3a2
 # 1e810a733192603f917e3492308a7e34923dc69193b01c6a317c97938830eac0151721249139666324912b6f6afd1f005c9f3f02329e930c8a1881077f7f9793a8910d5c048e933e83399c93939393939393939193900ae39293939393
 # 4a7331924a73319293934a7331924a73319293934a7331924a73319293934a7331924a73319293934a7331924a73319293934a7331924a7331929393
-# 
+#
 # 91abaaaaa3a6a3
 # 9b98334b824b962d92299e429e3ac691932692222b429a93af160bc3311a6c972e2d419713224f923c6f239293930791179193939293939334912592939393939393939391939393939393939393
 # 734b82734b829393734b82734b829393734b82734b829393734b82734b829393734b82734b829393734b82734b829393
-# 
+#
 # 93a0aaaaa3a3a5
 # 1e810d68b56cff63a8009a63a860da9193b42b29733c921c1957aa6ff270c103382ac4251c34cb2b535dba3c796c919393009f3e9b939395929393069a199b939393939393939391935d48c29393939393
 # 4d68b54d68b593934d68b54d68b593934d68b54d68b593934d68b54d68b593934d68b54d68b593934c68b54c68b59393
-# 
+#
 # 92a3a3a3a5abab
 # 188106358379ba55b2329755b26ada9193903d7841942d1cfac7fbdec2247004821e0e49821f4f6692013f329293932296049093939293939316971797939393939393939391939393939393939393
 # 463583463583939346358346358393934635834635839393463583463583939346358346358393934635834635839393
-# 
+#
 # 92a3a3a3a0a3a3
 # 1f813936ab6bd048b63a9548b66ada919354922b285f0e920b421192e12943c1271614a0290840a6176235b2240c229793930691189193939193939314923592939393939393939391939393939393939393
 # 7936ab7936ab93937936ab7936ab93937936ab7936ab93937936ab7936ab93937936ab7936ab93937936ab7936ab9393
-# 
+#
 # 92ababa3a3a3a6
 # d5803325b74718904f709b2b0c8d6716b13ec6919393355001789b030097498e48c037923897230f0e21923b2d0791939307b628b3939338973792939313918e93933a918e93933a929b91939393939393939393
 # 7325b77325b793937325b77325b793937325b77325b793937325b77325b793937325b77325b793937325b77325b79393
+
 
 # 93 -> 0  -> a3
 # 92 -> 1  -> a2
@@ -131,21 +128,23 @@ class f452(BaseParser):
 # 83 -> 16
 # 82 -> 17
 # 81 -> 18
-@register_parser(0x547) # TODO: 怪异编码
+@register_parser(0x547)  # TODO: 怪异编码
 class TODO547(BaseParser):
     def __init__(self, stocks: list[MARKET, str]):
         count = len(stocks)
         if count <= 0:
-            raise Exception('stocks count must > 0')
-        self.body = bytearray(struct.pack('<H', count))
-        
+            raise Exception("stocks count must > 0")
+        self.body = bytearray(struct.pack("<H", count))
+
         for market, code in stocks:
-            self.body.extend(struct.pack('<B6sHH', market.value, code.encode('gbk'), 22234, 2))
-        print('body: ', self.body.hex())
+            self.body.extend(
+                struct.pack("<B6sHH", market.value, code.encode("gbk"), 22234, 2)
+            )
+        print("body: ", self.body.hex())
 
     @override
     def deserialize(self, data):
-        print(struct.unpack('<H', data[:2]))
-        print('data: ', data.hex())
-            
+        print(struct.unpack("<H", data[:2]))
+        print("data: ", data.hex())
+
         return data
