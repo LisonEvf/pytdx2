57e621b6457f791a1603ffa86d54c5b27a240e5d
LisonEvf <LisonEvf@gmail.com>
2026-04-12 21:14:05 +0800

    fix: 修复 TdxClient 初始化方法并更新版本号至 0.1.1

--- opentdx/tdxClient.py
+++ opentdx/tdxClient.py
@@ -8,9 +8,11 @@ from opentdx.client.quotationClient import QuotationClient
 from opentdx.const import ADJUST, BLOCK_FILE_TYPE, CATEGORY, EX_MARKET, FILTER_TYPE, MARKET, PERIOD, SORT_TYPE
 
 class TdxClient:
-    def __enter__(self):
+    def __init__(self):
         self.quotation_client = QuotationClient(True, True)
         self.ex_quotation_client = exQuotationClient(True, True)
+        
+    def __enter__(self):
         self.quotation_client.connect().login()
         self.ex_quotation_client.connect().login()
         return self
